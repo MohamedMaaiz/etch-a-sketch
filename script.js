35e367010016345d70b8c33eb9cdd46cc9037102
Mohamed Maaiz <mohammadh3g@gmail.com>
2021-09-08 19:58:20 +0500

    cleaned all existing code to be more readble

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
-const grids = document.querySelector('.gridCollection'); //selecting the grids div
+//selectors
+const gridCollection = document.getElementById('grid-collection');
+const clearBTN = document.getElementById('clear-btn');
+const eraserBTN = document.getElementById('eraser-btn');
+const rangeSelector = document.getElementById('range-selector');
+const rangeNumber = document.getElementById('range-number');
 
-for (i=0; i < 256; i++){
-    const dive = document.createElement('div'); //creating the divs
-    dive.classList = 'divBox';
-    grids.appendChild(dive);
+
+//BTN selectors
+clearBTN.onclick = () => clearArts();
+eraserBTN.onclick = () => eraseOnHover();
+rangeSelector.onchange = () => rangechanger();
+
+//////////DEFAULT//////////
+gridGenerate(16);
+
+//generates the divs
+function gridGenerate (gridNumber) {
+    for (i=0; i < gridNumber * gridNumber; i++){
+        const oneDiv = document.createElement('div');
+        oneDiv.addEventListener('mouseover', () => {oneDiv.classList.add('divHover')});
+        oneDiv.classList = 'generated';
+        gridCollection.appendChild(oneDiv);
+    }
 }
 
-//selecting the dives 
-var dives = document.querySelectorAll('.divBox');
-//adding color class to divs
-dives.forEach(div => div.addEventListener('mouseover', function() {div.classList.add('divHover')}));
-
-//selecting the eraze button
-const clear = document.getElementById('clear');
-//erazing
-clear.addEventListener('click', () => {
-    dives.forEach(div => div.addEventListener('mouseover', function() {div.classList.toggle('divHover')}));
-});
-
-//selecting clear button
-const clearAllBtn = document.getElementById('clearAllBtn');
-//cleaning
-clearAllBtn.addEventListener('click', () => {
+//clean all arts
+function clearArts() {
    var allColored = document.querySelectorAll('.divHover');
    for(i=0; i < allColored.length; i++){
        allColored[i].classList.remove('divHover');
    };
-});
-//selecting number slider
-const divNum = document.getElementById('divNum');
-//getting range
-var divNumSize = document.getElementById('divNumSize');
-//showing the number
-divNumSize.innerHTML = divNum.value;
-divNum.oninput = function() {
-    divNumSize.innerHTML = this.value;
-}
+};
+
+//erase on mouse hover
+function eraseOnHover() {
+    const dives = document.querySelectorAll('.generated');
+        dives.forEach(div => div.addEventListener('mouseover', function() {
+            div.classList.toggle('divHover')
+        }));
+};
+
+//changes the div box number
+function rangechanger() {
+    rangeSelector.innerHTML = rangeNumber.value;
+    rangeSelector.oninput = function() {
+        rangeNumber.innerHTML = this.value;
+    }
+};
