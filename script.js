c0b48e2f67404cf6b574d0455f21c71eea8f070f
Mohamed Maaiz <mohammadh3g@gmail.com>
2021-09-18 01:31:44 +0500

    completed the project, but neet to add profile information

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 const gridCollection = document.getElementById('grid-collection');
 const clearBTN = document.getElementById('clear-btn');
 const eraserBTN = document.getElementById('eraser-btn');
-const rangeSelector = document.getElementById('range-selector');
-const rangeNumber = document.getElementById('range-number');
-const colorPicker = document.getElementById('color-picker')
-const mixBTN = document.getElementById('mix-btn')
+const sizeSelector = document.getElementById('size-selector');
+const sizeNumber = document.getElementById('size-number');
+const colorPicker = document.getElementById('color-picker');
+const mixBTN = document.getElementById('mix-btn');
+const grayBTN = document.getElementById('gray-btn');
 
 //BTN selectors
+grayBTN.onclick = () => shader();
 clearBTN.onclick = () => clearArts();
 mixBTN.onclick = () => colorMixer();
-// mixBTN.onclick = () => changeColor(currentMode = 'mix');
 eraserBTN.onclick = () => eraseOnHover();
-rangeSelector.onchange = () => rangechanger();
+sizeSelector.onmousemove = () => sizeChangerTxt();
+sizeSelector.onchange = () => sizeChanger();
 colorPicker.onchange = () => userColor();
 
 //////////DEFAULT//////////
-// let defaultColor = '#000000';
-let dives;
-let currentMode = 'black';
+let currentMode = 'default';
 gridGenerate(16);
 
 //generates the divs
@@ -34,87 +34,111 @@ function gridGenerate (gridNumber) {
 }
 
 //changing the mode
-//change the color on color pick
 function changeColor() {
-    console.log(currentMode)
-    
-    switch (currentMode) {
-        case 'balck':
-            this.style.backgroundColor = '#ffffff'
-            break;
 
+    switch (currentMode) {
         case 'mix':
             this.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
             break;
 
-        case 'user':
-            // console.log(colorPicker.value)
-            // this.style.backgroundColor = colorPicker.value;
+        case 'shade':
+            let colorCode = '';
+            if (this.style.backgroundColor.match(/rgb/)){
+                colorCode = Number(this.style.backgroundColor.slice(-4, -1));
+                this.style.backgroundColor = `rgb(${colorCode-20} ${colorCode-20} ${colorCode-20})`
+            } else {
+                this.style.backgroundColor = `rgb(${200} ${200} ${200})`
+            }
+            break;
+
+        case 'erase':
+            this.style.backgroundColor = ''
             break;
     
         default:
             this.style.backgroundColor = colorPicker.value
             break;
     }
+}
 
+//shading gray
+let shadeMode = false;
+function shader() {
+    if (shadeMode == true){
+        currentMode = temp;
+        currentMode = 'default';
+        grayBTN.classList.remove('buttonActive');
+        shadeMode = false;
+    } else {
+        currentMode = 'shade';
+        eraserBTN.classList.remove('buttonActive');
+        eraser = false;
+        mixBTN.classList.remove('buttonActive');
+        colorMixMode = false;
+        grayBTN.classList.add('buttonActive');
+        shadeMode = true;
+    }
 }
 
 //erase on mouse hover
-// have to chage the menual number
+let temp = '';
 let eraser = false;
 function eraseOnHover() {
-    let mode;
     if (eraser == true) {
-        mode = '#ffffff';
+        currentMode = temp;
         eraser = false;
     } else {
-        mode = '';
+        temp = currentMode;
+        currentMode = 'erase';
         eraser = true;
     }
-    divSelect();
     eraserBTN.classList.toggle('buttonActive');
-    dives.forEach(div => div.addEventListener('mouseover', function(e) {
-        e.target.style.backgroundColor = mode;
-    }));
-};
+}
 
 //changing the color when user chagnes it
 function userColor() {
     currentMode = 'default'
+    currentMode = temp;
+    grayBTN.classList.remove('buttonActive');
+    shadeMode = false;
+    mixBTN.classList.remove('buttonActive');
+    colorMixMode = false;
+    eraserBTN.classList.remove('buttonActive');
+    eraser = false;
 }
-
-
-//selecting all the generated dives
-function divSelect () {
-    dives = document.querySelectorAll('.generated');
-}
-
 
 //clean all arts
 function clearArts() {
-    divSelect();
+    let dives = document.querySelectorAll('.generated');
     dives.forEach(div => div.style.backgroundColor = '')
-};
+}
 
 // color Mixer
 let colorMixMode = false;
 function colorMixer() {
     if (colorMixMode == true) {
-        currentMode = 'default'
+        currentMode = temp;
+        mixBTN.classList.remove('buttonActive');
+        currentMode = 'default';
         colorMixMode = false;
     } else { 
         currentMode = 'mix'
-        colorMixMode = true
+        grayBTN.classList.remove('buttonActive');
+        shadeMode = false;
+        colorMixMode = true;
+        eraserBTN.classList.remove('buttonActive');
+        eraser = false;
+        mixBTN.classList.add('buttonActive')
     }
-    mixBTN.classList.toggle('buttonActive');
+    // mixBTN.classList.toggle('buttonActive');
 }
 
 //changes the div box number
-function rangechanger() {
-    rangeSelector.innerHTML = rangeNumber.value;
-    rangeSelector.oninput = function() {
-        rangeNumber.innerHTML = this.value;
-    }
+function sizeChanger() {
     gridCollection.innerHTML = '';
-    gridGenerate(rangeNumber.innerHTML);
-};
+    gridGenerate(sizeSelector.value)
+}
+
+function sizeChangerTxt() {
+    sizeNumber.innerHTML = sizeSelector.value + " X " + sizeSelector.value;
+}
